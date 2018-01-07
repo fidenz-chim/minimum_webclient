3f6fec01d7dc77b14bba9948f71b5360e7d5ee45
ChimH <chim@fidenz.com>
2018-01-07 22:00:05 +0530

    update changes

--- samplejs.js
+++ samplejs.js
@@ -5,7 +5,8 @@ document.getElementById('sampleTextInput').value = "Sample text input pre-loaded
 //Event bindings
 document.getElementById('sampleInputFile').addEventListener('change',onOpenSampleFile);
 document.getElementById('sampleButton').addEventListener('click',onSampleButtonClick);
-
+document.getElementById('formatStringButton').addEventListener('click',onFormatStringButtonClick);
+formatStringButton
 
 //Event listener funtions
 function onOpenSampleFile(event){
@@ -20,5 +21,34 @@ function onOpenSampleFile(event){
 
 function onSampleButtonClick(){
     var testVal = document.getElementById('sampleTextInput').value;
-    alert(testVal);
+
+    var temp = document.createElement("div");
+    temp.innerHTML = getHTMLBlock(testVal);
+
+    var parent = document.getElementById('lowerArea');
+    parent.appendChild(temp);
+
+}
+
+function getHTMLBlock(content){
+    var html;
+    html = '<div>' + content + '</div>';
+    console.log(html);
+    return html;
+}
+
+String.prototype.format = function() {
+  a = this;
+  for (k in arguments) {
+      var rep = '/{' + k + '}/g';
+      console.log(rep);
+      console.log(arguments[k]);
+    a = a.replace(rep, arguments[k])
+  }
+  return a
+}
+
+function onFormatStringButtonClick(){
+    var str = "hello {0}, how are you {0}, {1} {0}".format("John","Val");
+    console.log(str);
 }
